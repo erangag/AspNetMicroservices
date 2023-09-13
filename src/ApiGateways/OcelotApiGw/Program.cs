e2eb48fc74f849b276738348675b8c133c0c4afd
erangag <erangagunarathna@gmail.com>
2023-09-14 09:46:29 +1200

    Added Shopping.Aggregator

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,6 +1,19 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Logging.ClearProviders().AddConsole().AddDebug();
+var temp = builder.Environment.EnvironmentName;
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
 
 app.Run();
